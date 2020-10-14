40dc42bd12b06187a6feb0b803503c62b1fd2954
Vil-Kisser Torres <viltorres21@gmail.com>
2020-10-14 17:00:42 +0800

    commit sample react

--- src/App.js
+++ src/App.js
@@ -3,24 +3,21 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          hola
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+          Click me!
         </a>
       </header>
     </div>
   );
+  
 }
 
 export default App;
